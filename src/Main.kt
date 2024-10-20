5b8aaedf0dc9384db152b6314368a0b3863031c8
Harry <168691814+waimea-hwstringer@users.noreply.github.com>
2024-10-21 10:56:22 +1300

    movable pieces and colour

--- src/Main.kt
+++ src/Main.kt
@@ -18,21 +18,29 @@
 
 import kotlin.random.Random
 
+var currentPlayer = 1
+
 fun main() {
 
     println("===================")
     println("WELCOME TO OLD GOLD")
     println("===================")
+
     /* 0 = empty | X = stone | $ = gold */
 
-    generateNewGame()
+    val boardLayout = generateNewGame() // Starts the game by setting the board layout
 
-    showGameBoard()
+    showGameBoard(boardLayout) // Displays the current board layout in the table
+
+    getInput(boardLayout)
+
 }
 
 fun generateNewGame(): MutableList<Char> {
 
     val boardLayout = MutableList(20){'0'}
+
+    currentPlayer = 1
 
     var xNo = 0
     var goldPlaced = false
@@ -60,30 +68,54 @@ fun generateNewGame(): MutableList<Char> {
 
 fun showGameBoard(boardLayout: MutableList<Char> ) {
 
-    //Build the top
+    //Build the top line
     var x = 1
-    print("╤═══".repeat(boardLayout.size))
+    print("╤════".repeat(boardLayout.size))
     println("╤")
 
     //Show the index number
     for (item in boardLayout) {
-        print("│ %-1d ".format(x))
+        print("│ %-2d ".format(x))
         x++
     }
     println("│")
 
-    //Build the middle
-    print("╪═══".repeat(boardLayout.size))
+    //Build the middle line
+    print("╪════".repeat(boardLayout.size))
     println("╪")
 
-    //Show the pets
+    //Show the pieces on the board
     for (item in boardLayout) {
-        print("│ %s ".format(item))
+
+        val colouredItem = when (item) {
+            'X' -> item.toString().red()
+            '$' -> item.toString().yellow()
+            else -> item.toString().grey()
+        }
+
+        print("│ %-2s  ".format(colouredItem))
         x++
     }
     println("│")
 
-    //Build the bottom
-    print("╧═══".repeat(boardLayout.size))
+    //Build the bottom line
+    print("╧════".repeat(boardLayout.size))
     println("╧")
+}
+
+fun getInput(boardLayout: MutableList<Char>) {
+
+    print("Player $currentPlayer's turn: What piece would you like to move? ")
+    val pieceToMove = readln().toInt() -1 // Old pos of piece (-1 as index starts at 0 not 1)
+
+    print("Where do you want piece $pieceToMove to go? ")
+    val pieceNewLocation = readln().toInt() -1 // New pos of piece (-1 as index starts at 0 not 1)
+
+
+    val temp = boardLayout[pieceNewLocation]
+
+    boardLayout[pieceNewLocation] = boardLayout[pieceToMove]
+
+    boardLayout[pieceToMove] = temp
+
 }
