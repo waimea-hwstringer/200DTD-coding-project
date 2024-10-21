bf5e166dcec254e613f7be02306015c231afc6ca
Harry <168691814+waimea-hwstringer@users.noreply.github.com>
2024-10-22 09:56:26 +1300

    actually worsk!

--- src/Main.kt
+++ src/Main.kt
@@ -18,21 +18,49 @@
 
 import kotlin.random.Random
 
-var currentPlayer = 1
+var name1 = ""
+var name2 = ""
+
+var currentPlayer = ""
+
+var won = false
 
 fun main() {
 
-    println("===================")
-    println("WELCOME TO OLD GOLD")
-    println("===================")
-
     /* 0 = empty | X = stone | $ = gold */
+
+    getStartingInfo()
 
     val boardLayout = generateNewGame() // Starts the game by setting the board layout
 
-    showGameBoard(boardLayout) // Displays the current board layout in the table
+    while (!won) {
+        showGameBoard(boardLayout) // Displays the current board layout
 
-    getInput(boardLayout)
+        getInput(boardLayout)
+
+        switchPlayer()
+
+    }
+
+    switchPlayer() //Must switch player back as after input is received player is switched automatically
+    println("Congrats $currentPlayer, You won!")
+
+}
+
+fun getStartingInfo(){
+    println("===========================================")
+    println("            WELCOME TO OLD GOLD")
+    println("===========================================")
+
+    print("Howdy there partners! What is your name?, player one? :")
+    name1 = readln()
+
+    print("And what is your name, player two? :")
+    name2 = readln()
+
+    println("How big do you want the board to be? :")
+
+    return
 
 }
 
@@ -40,7 +68,7 @@ fun generateNewGame(): MutableList<Char> {
 
     val boardLayout = MutableList(20){'0'}
 
-    currentPlayer = 1
+    currentPlayer = name1
 
     var xNo = 0
     var goldPlaced = false
@@ -105,17 +133,34 @@ fun showGameBoard(boardLayout: MutableList<Char> ) {
 
 fun getInput(boardLayout: MutableList<Char>) {
 
-    print("Player $currentPlayer's turn: What piece would you like to move? ")
+    print("$currentPlayer's turn: What piece would you like to move? ")
     val pieceToMove = readln().toInt() -1 // Old pos of piece (-1 as index starts at 0 not 1)
 
-    print("Where do you want piece $pieceToMove to go? ")
-    val pieceNewLocation = readln().toInt() -1 // New pos of piece (-1 as index starts at 0 not 1)
+    if (pieceToMove == 0) {
+        println("$currentPlayer removed piece 1!")
 
+        if (boardLayout[0] == '$') {
+            won = true
+        }
+    }
 
-    val temp = boardLayout[pieceNewLocation]
+    else {
+        print("Where do you want piece ${pieceToMove+1} to go? ")
+        val pieceNewLocation = readln().toInt() -1 // New pos of piece (-1 as index starts at 0 not 1)
 
-    boardLayout[pieceNewLocation] = boardLayout[pieceToMove]
+        boardLayout[pieceNewLocation] = boardLayout[pieceToMove]
+    }
 
-    boardLayout[pieceToMove] = temp
+    boardLayout[pieceToMove] = '0'
 
+}
+
+fun switchPlayer() {
+
+    if (currentPlayer == name1) {
+        currentPlayer = name2
+    }
+    else {
+        currentPlayer = name1
+    }
 }
